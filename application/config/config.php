79144591ea686bb795083268609b6e25f95fa281
CrisM <czmalajos@gmail.com>
2017-04-05 20:58:22 +0800

    Fixs .htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,8 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php?';
+$config['uri_protocol'] = "QUERY_STRING";
 
 /*
 |--------------------------------------------------------------------------
@@ -52,7 +53,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+// $config['uri_protocol']	= 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
